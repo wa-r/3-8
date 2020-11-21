18cabe56acd34cc3a2de361c08780614668d0022
wa-r <nyuuuuun14@gmail.com>
2020-11-21 12:42:26 +0000

    [Add]サクセスメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,8 +14,13 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    if book.save
+      redirect_to book_path(book.id), notice: 'Book was successfully created.'
+    else
+      @books = Book.all.order(created_at: :asc)
+      @book = book
+      render :index
+    end
   end
 
   def edit
@@ -24,14 +29,18 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      redirect_to book_path(book.id), notice: 'Book was successfully updated.'
+    else
+      @book = book
+      render :edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to books_path
+    redirect_to books_path, notice: 'Book was successfully destroyed.'
   end
 
   private
